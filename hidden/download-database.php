fec54f2bd6f321954b2a845354c8fa1b16ff37dc
LolHero877 <79245167+LolHero877@users.noreply.github.com>
2022-06-25 03:44:24 +0300

    download csv wishlist

---
+++ hidden/download-database.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST["Download"])){
+    $uid = $_POST["uid"];
+
+    require_once './dbh-hidden.php';
+    require_once './functions-hidden.php';
+    require_once './preference-functions-hidden.php';
+
+    $cmd = "SELECT * FROM beverages WHERE id_produs IN (SELECT id_produs_wish as id_produs FROM wishlist WHERE id_user_wish = $uid) order by id_produs";
+    $cmd = "copy (" . $cmd . ") to 'D:/Downloads/Xamp/htdocs/SoDrOProject/tmp/wishlist.csv' DELIMITER ',' csv header";
+    pg_query($conn, $cmd);
+    $path = 'D:/Downloads/Xamp/htdocs/SoDrOProject/tmp/wishlist.csv';
+
+    header("location: ./download-hidden-two.php?path=$path");
+    exit();
+
+}
--- wishlist.php
+++ wishlist.php
@@ -9,21 +9,39 @@
 <head>
     <title>Wishlist</title>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
 </head>
 
 <body>
-
 <?php
 include_once 'header.php';
 ?>
 <div class="contentdesk">
     <div class="formbox">
         <h3>Wishlistul lui <?php echo $_SESSION['user']; ?></h3>
+
+            <table style="width: 25%; margin-left: auto; margin-right: auto;">
+                <tbody>
+                <tr>
+                    <td><div class="formbox2">
+        <form action="./hidden/download-database.php" method="post">
+            <input type="submit" value="Download as CSV &#11015;"  name="Download" />
+            <input type="hidden" name="uid" value="<?php
+            require_once './hidden/dbh-hidden.php';
+            require_once './hidden/functions-hidden.php';
+            require_once './hidden/preference-functions-hidden.php';
+            echo getID($conn, $_SESSION['user']);
+            ?>">
+
+
+
+        </form></div></td>
+        </tr>
+        </tbody>
+        </table>
+
         <?php
-        require_once './hidden/dbh-hidden.php';
-        require_once './hidden/functions-hidden.php';
 
         echo allWishlistDesk($conn, $_SESSION['user']);
         ?>
