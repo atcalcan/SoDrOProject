178ef8c2f5a0bb538321da13edc642eaf8004cb3
LolHero877 <79245167+LolHero877@users.noreply.github.com>
2022-06-25 13:27:09 +0300

    lists and wishlist

---
+++ sqls/users_lists.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (1, 2, 'test');
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (2, 13, 'Party la Giku');
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (3, 13, 'Suc pt ala micu');
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (4, 13, 'Mirela');
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (5, 13, 'Mamaie');
+INSERT INTO public.users_lists (list_id, uid_lists, list_name) VALUES (6, 13, 'Ziua lui stefanel');
--- sqls/wishlist.sql
+++ sqls/wishlist.sql
@@ -1,234 +1,118 @@
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 1);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 5);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 9);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 4);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 17);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 31);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 3);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 5);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 14);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 32);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 38);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 37);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 21);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (5, 22);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 9);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 18);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 4);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 16);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 17);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 19);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 34);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (3, 1);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 21);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 22);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 25);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 11);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 12);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 10);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 20);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 26);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 8);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 24);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 13);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 7);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 29);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 39);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 40);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (4, 36);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 27);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 25);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 26);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 1);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 9);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (6, 34);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (7, 4);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (7, 19);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (7, 33);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (8, 31);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (8, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (8, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (8, 3);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (8, 5);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 23);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 15);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 12);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 10);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 29);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 24);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 35);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (9, 28);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 37);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 7);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 20);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 9);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 40);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 21);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (10, 5);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 26);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 28);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 2);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 20);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 25);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 27);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 7);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 35);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 8);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 37);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 36);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 24);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 29);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 5);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 9);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 10);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 40);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 3);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 22);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 32);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 31);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 39);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 38);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 30);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 21);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (11, 23);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 20);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 17);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 15);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 12);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 11);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 6);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 31);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 32);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 37);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 36);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 7);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (12, 8);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 27);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 7);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 8);
-INSERT INTO public.wishlist (id_user_wish, id_produs_wish)
-VALUES (2, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 1);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 5);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 9);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 4);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 17);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 31);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 3);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 5);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 14);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 32);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 38);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 21);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (5, 22);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 9);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 18);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 4);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 16);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 17);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 19);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 34);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (3, 1);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 21);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 22);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 25);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 11);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 12);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 10);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 20);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 26);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 8);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 24);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 13);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 7);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 29);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 39);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 40);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (4, 36);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 27);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 25);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 26);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 1);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 9);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (6, 34);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (7, 4);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (7, 19);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (7, 33);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (8, 31);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (8, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (8, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (8, 3);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (8, 5);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 23);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 15);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 12);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 10);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 29);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 24);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 35);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (9, 28);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 7);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 20);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 9);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 40);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 21);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (10, 5);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 26);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 28);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 2);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 20);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 25);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 27);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 7);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 35);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 8);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 36);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 24);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 29);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 5);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 9);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 10);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 40);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 3);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 22);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 32);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 31);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 39);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 38);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 30);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 21);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (11, 23);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 20);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 17);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 15);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 12);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 11);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 6);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 31);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 32);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 36);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 7);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (12, 8);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 27);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 7);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 8);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (2, 37);
+INSERT INTO public.wishlist (id_user_wish, id_produs_wish) VALUES (13, 40);
